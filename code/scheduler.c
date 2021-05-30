db45826b85b4d132ba03372934695d590e94e487
Mostafa-wael <56788883+Mostafa-wael@users.noreply.github.com>
2021-05-30 07:02:53 +0200

    initial commit

---
+++ code/scheduler.c
@@ -0,0 +1,11 @@
+#include "headers.h"
+
+int main(int argc, char *argv[])
+{
+    initClk();
+
+    //TODO: implement the scheduler.
+    //TODO: upon termination release the clock resources.
+
+    destroyClk(true);
+}
