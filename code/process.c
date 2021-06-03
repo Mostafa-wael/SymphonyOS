bfbf3353db2b7271a94d78ff964c545e9bad2e4b
moste00 <48567303+moste00@users.noreply.github.com>
2021-06-03 03:55:26 +0200

    Add files via upload

--- code/process.c
+++ code/process.c
@@ -2,20 +2,43 @@
 
 /* Modify this file as needed*/
 int remainingtime;
+bool process_running ;
 
+void on_interruption_sleep(int);
+void on_resumption_awaken(int);
 int main(int agrc, char *argv[])
 {
     initClk();
 
-    //TODO The process needs to get the remaining time from somewhere
-    //remainingtime = ??;
+    remainingtime = atoi(argv[0]);
+
+    signal(SIGUSR1,on_interruption_sleep);
+    signal(SIGUSR2,on_resumption_awaken);
+    process_running = true ;
+    int prev = getClk();
     while (remainingtime > 0)
-    {
-        
-        // remainingtime = ??;
+    {        
+        int curr = getClk();
+
+        if(prev != curr){
+            if(process_running) remainingtime-- ;
+            
+            prev = curr ;
+        }
     }
 
+    kill(getppid(),SIGUSR2);
+    
     destroyClk(false);
 
     return 0;
+}
+
+void on_interruption_sleep(int signum){
+    process_running = false ;
+    signal(SIGUSR1,on_interruption_sleep);
+}
+void on_resumption_awaken(int signum){
+    process_running = true ;
+    signal(SIGUSR2,on_resumption_awaken);
 }
