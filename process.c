c3f1e8e990a57f650cc59ed255213352417766d3
Mostafa-wael <56788883+Mostafa-wael@users.noreply.github.com>
2021-06-04 09:33:25 +0200

    Added more comments

---
+++ process.c
@@ -0,0 +1,55 @@
+#include "headers.h"
+
+// this file is responsible for simulating the process as if it is running
+int remainingtime;    // until the process finishes
+bool process_running; // runnung or not
+
+void on_interruption_sleep(int);
+void on_resumption_awaken(int);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//****************************************** Main loop *****************************************//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int agrc, char *argv[])
+{
+    initClk();
+
+    remainingtime = atoi(argv[0]); // take the remaining time as an argumnet 
+
+    signal(SIGUSR1, on_interruption_sleep); // make the process sleep if it has recieved a sleeping signal 
+    signal(SIGUSR2, on_resumption_awaken); // awaking the process if it has recieved an awaken signal
+    process_running = true;
+
+    int prev = getClk();
+    while (remainingtime > 0) // decremnt the remaining time every one clock tick
+    {
+        int curr = getClk();
+
+        if (prev != curr)
+        {
+            if (process_running)
+                remainingtime--;
+
+            prev = curr;
+        }
+    }
+    kill(getppid(), SIGUSR2); // kill the processa wehn its remaining time is zero!
+
+
+    destroyClk(false);
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//****************************************** Utilities *****************************************//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+void on_interruption_sleep(int signum) // make the process sleep if it has recieved a sleeping signal 
+{
+    process_running = false;
+    signal(SIGUSR1, on_interruption_sleep);
+}
+void on_resumption_awaken(int signum) // awaking the process if it has recieved an awaken signal
+{
+    process_running = true;
+    signal(SIGUSR2, on_resumption_awaken);
+}
---
+++ process_generator.c
@@ -0,0 +1,173 @@
+#include "headers.h"
+
+void clearResources(int);
+int MsgQID;
+
+
+int main(int argc, char *argv[]) // file name, scheduling algorithm
+{
+    signal(SIGINT, clearResources);
+    // test the passed arguments
+    printf("\nNumber of passed arguments to the process_generator is %d\n", argc);
+    for (int i = 1; i < argc; i++)
+        printf("passed argument number %d is: %s\n", i, argv[i]);
+    //****************************************************************
+    // 1. Read the input files and save them
+    char *FileName = argv[1];
+    FILE *pFile;
+    pFile = fopen(FileName, "r"); // read mode
+    if (pFile == NULL)
+    {
+        perror("\nError while opening the file:");
+        exit(EXIT_FAILURE);
+    }
+    //******************************
+   // int tableSize = 100;                                             // indicates the max arrival time
+   // comingProcess **procTable = createComingProcessTable(tableSize); // creates a table (hashmap) for the commingProcesses
+    char buff[50]; 
+    unsigned int number_processes = 0; 
+    while (fgets(buff, sizeof(buff), pFile) != NULL)
+    {
+        if (buff[0] != '#')
+            number_processes++;
+
+    }
+    //check 
+    printf("%d\n",number_processes);
+
+    // return to beginning of the file again
+    fseek( pFile, 0, SEEK_SET );
+    
+    // create array of processes 
+    comingProcess * procTable = (struct comingProcess *) malloc(sizeof(struct comingProcess) * number_processes);
+     
+    unsigned int index = 0;      
+                                              
+    while (fgets(buff, sizeof(buff), pFile) != NULL)
+    {
+        if (buff[0] == '#')
+            continue;
+
+        sscanf(buff, "%d\t%d\t%d\t%d",
+               &(procTable[index].id),
+               &(procTable[index].arrivaltime),
+               &(procTable[index].runningtime),
+               &(procTable[index].priority));
+
+        index++;
+    }
+    fclose(pFile);
+    // print the processes
+    printf("#id arrival runtime priority\n");
+
+    //check
+    for (int i = 0; i < number_processes; i++)
+    {
+        printf("%d , %d ,%d , %d \n",procTable[i].id,procTable[i].arrivaltime,procTable[i].runningtime,procTable[i].priority);
+    }
+    
+    //****************************************************************
+    // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
+    char *chosen_algorithm = argv[2];
+    char *additionalParameters = argv[3]; // like quantum in case of RR
+    printf("%s", chosen_algorithm);
+    // 3. Initiate and create the scheduler and clock processes.
+    int pidSch = fork();
+    if (pidSch == 0)
+    {
+        printf("ProcessGenerator : I have started the scheduler");
+        char *arr[] = {chosen_algorithm, additionalParameters, NULL};
+        execv("./scheduler.out", arr);
+
+    }
+    int pidCLK = fork();
+    if (pidCLK == 0)
+    {
+        printf("ProcessGenerator : I have started the clock");
+        char *arr[] = {NULL};
+        execv("./clk.out", arr);
+    }
+    // create keys for the message queue
+    //key_t KeyID = ftok("keyfile", SHMSGKEY); // the key of the Queue
+    // get the message queues
+    MsgQID = msgget(ftok("keyfile", 'S'), 0666 | IPC_CREAT); 
+    //create messageReceived queue and return id
+    // check for any errors
+    if (MsgQID == -1) // if messageid == -1, it failed to create the queue
+    {
+        perror("Error in create");
+        exit(-1);
+    }
+
+    // 4. Use this function after creating the clock process to initialize clock.
+    initClk();
+    // TODO Generation Main Loop
+    //****************************************************************
+    // 5. Create a data structure for processes and provide it with its parameters.
+    // done while reading the file
+    //****************************************************************
+    // 6. Send the information to the scheduler at the appropriate time.
+    struct msgbuff currentProcesses;
+    // shared memory 4 byte and id ftok keyfile 'M'
+    // int * msgqueue   
+    int MsqQIDszSHMID = shmget(ftok("keyfile", 'M'),4,0666|IPC_CREAT);
+    int* MsgQIDsz   = (int*)shmat(MsqQIDszSHMID,0,0);
+    
+    int prevTime = getClk();
+    unsigned int current_index = 0;
+    //procTable[number_processes-1].arrivaltime > getClk()
+    
+    while (1)
+    {
+        int currentTime = getClk();
+        if (currentTime != prevTime) // add this condition to avoid printing the time a lot
+        {
+            printf("ProcessGenerator : Current Time is %d\n", currentTime);
+            if (procTable[current_index].arrivaltime == currentTime)
+            {            
+                while (procTable[current_index].arrivaltime == currentTime)
+                {
+                    printf("ProcessGenerator : There is a process at Time %d\n", currentTime);
+
+                        // send the value to the server
+                        currentProcesses.mtype = 7;
+                        currentProcesses.currentProcess = procTable[current_index];
+                        int sendValue = msgsnd(MsgQID, &currentProcesses, sizeof(struct msgbuff) - sizeof(long), !IPC_NOWAIT);
+                        if (sendValue == -1)
+                            perror("Errror in sent");
+                        else{
+                            (*MsgQIDsz)++;
+                            printf("ProcessGenerator : %d, %d, %d, %d\n",
+                            procTable[current_index].id,
+                            procTable[current_index].arrivaltime,
+                            procTable[current_index].runningtime,
+                            procTable[current_index].priority);
+                        }    
+                        current_index++;
+                }  
+            kill(pidSch,SIGUSR1);
+            }
+            prevTime = currentTime;
+        }
+    }
+    // 
+    //
+    //
+    //****************************************************************s
+    // 7. Clear clock resources
+   
+    
+}
+
+void clearResources(int signum)
+{
+    //TODO Clears all resources in case of interruption
+    // remove the message queue from the kernel
+    msgctl(MsgQID, IPC_RMID, (struct msqid_ds *)0);
+    // kill all the processes
+    killpg(getpgrp(), SIGINT);
+    // re-set the signal hnadler
+    signal(SIGINT, clearResources);
+    destroyClk(true);
+    exit(0);
+}
